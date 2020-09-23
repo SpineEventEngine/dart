6c0511f9e496954433bb00891b6868e9045c59ee
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2020-09-23 14:51:36 +0300

    Update Gradle and Spine deps

--- version.gradle.kts
+++ version.gradle.kts
@@ -29,6 +29,6 @@
  * already in the root directory.
  */
 
-val spineBaseVersion: String by extra("1.5.14")
-val spineWebVersion: String by extra("1.5.14")
-val versionToPublish: String by extra("1.5.14")
+val spineBaseVersion: String by extra("1.6.1")
+val spineWebVersion: String by extra("1.6.0")
+val versionToPublish: String by extra("1.6.0")
