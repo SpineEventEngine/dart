06228f863d7fb451d7c6ad13aacd4206811f99df
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2020-12-04 13:23:20 +0200

    Update config and Gradle

--- integration-tests/client-test/integration-test/client_test.dart
+++ integration-tests/client-test/integration-test/client_test.dart
@@ -18,6 +18,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import 'package:spine_client/client.dart';
 import 'package:spine_client/spine_client.dart';
 import 'package:spine_client/uuids.dart';
 import 'package:spine_client/web_firebase_client.dart';
@@ -26,99 +27,116 @@ import 'package:test/test.dart';
 import 'endpoints.dart';
 import 'firebase_app.dart';
 import 'spine/web/test/given/commands.pb.dart';
+import 'spine/web/test/given/events.pb.dart';
 import 'spine/web/test/given/task.pb.dart';
 import 'types.dart' as testTypes;
 
 @TestOn("browser")
 void main() {
 
-    group('BackendClient should', () {
-        ActorRequestFactory requestFactory;
-        BackendClient client;
+    group('Client should', () {
+        Clients clients;
         FirebaseClient firebaseClient;
+        UserId actor;
 
         setUp(() {
             var database = FirebaseApp().database;
             firebaseClient = WebFirebaseClient(database);
-            client = BackendClient(BACKEND,
-                                   firebase: firebaseClient,
-                                   typeRegistries: [testTypes.types()]);
-            var actor = UserId();
-            actor.value = newUuid();
-            requestFactory = ActorRequestFactory(actor);
+            clients = Clients(BACKEND,
+                              firebase: firebaseClient,
+                              typeRegistries: [testTypes.types()]);
+            actor = UserId()..value = newUuid();
+        });
+
+        tearDown(() {
+            clients.cancelAllSubscriptions();
         });
 
         test('send commands and obtain query data', () async {
+            print('----> Starting test.');
             var taskId = TaskId()
                 ..value = newUuid();
             var cmd = CreateTask()
                 ..id = taskId
                 ..name = 'Task name'
                 ..description = "long";
-            await client.post(requestFactory.command().create(cmd));
-            var query = requestFactory.query().all(Task());
-            var tasks = await client.fetch<Task>(query).toList();
+            var client = clients.onBehalfOf(actor);
+            var request = client.command(cmd);
+            request.observeEvents(TaskCreated());
+            var subscriptions = request.post();
+            expect(subscriptions, hasLength(equals(1)));
+            print('----> Waiting for event.');
+            await subscriptions.first.events.first;
+            print('----> Received event.');
+            var tasks = await client.select(Task())
+                                    .post()
+                                    .toList();
+            print('----> Received entity states.');
             expect(tasks, hasLength(greaterThanOrEqualTo(1)));
             var matchingById = tasks.where((task) => task.id == taskId);
             expect(matchingById, hasLength(1));
         });
 
         test('query server directly', () async {
-            client = BackendClient(BACKEND,
-                                   queryMode: QueryMode.DIRECT,
-                                   endpoints: Endpoints(
-                                       query: 'direct-query'
-                                   ));
+            clients = Clients(BACKEND,
+                              queryMode: QueryMode.DIRECT,
+                              endpoints: Endpoints(
+                                  query: 'direct-query'
+                              ));
             var taskId = TaskId()
                 ..value = newUuid();
             var cmd = CreateTask()
                 ..id = taskId
                 ..name = 'Task name'
                 ..description = "long";
-            await client.post(requestFactory.command().create(cmd));
-            var query = requestFactory.query().all(Task());
-            var tasks = await client.fetch<Task>(query).toList();
+            clients.onBehalfOf(actor)
+                   .command(cmd)
+                   .postAndForget();
+            var tasks = await clients.asGuest()
+                                     .select(Task())
+                                     .post()
+                                     .toList();
             expect(tasks, hasLength(greaterThanOrEqualTo(1)));
             var matchingById = tasks.where((task) => task.id == taskId);
             expect(matchingById, hasLength(1));
         });
 
         test('subscribe to entity changes', () async {
-            // Subscribe to the `Task` changes.
-            var topic = requestFactory.topic().all(Task());
-            Subscription<Task> entitySubscription = await client.subscribeTo(topic);
+            var client = clients.onBehalfOf(actor);
+            StateSubscription<Task> entitySubscription =
+                    client.subscribeTo(Task())
+                          .post();
 
-            // Listen to the `itemAdded` event.
             var taskName = "";
             Stream<Task> itemAdded = entitySubscription.itemAdded;
             itemAdded.listen((task) => taskName = task.name);
             var taskId = TaskId()
                 ..value = newUuid();
-
-            // Send `CreateTask` command.
             var createTaskCmd = CreateTask()
                 ..id = taskId
                 ..name = 'Task name'
                 ..description = "long";
-            await client.post(requestFactory.command().create(createTaskCmd));
+            var createTaskRequest = client.command(createTaskCmd);
+            var taskCreatedEvents = createTaskRequest.observeEvents(TaskCreated());
+            createTaskRequest.post();
 
-            // Check the event is actually fired.
+            var newTaskEvent = await taskCreatedEvents.first;
+            expect(newTaskEvent.id, equals(taskId));
             expect(taskName, equals(createTaskCmd.name));
 
-            // Listen to the `itemChanged` event.
             var newTaskName = "";
             Stream<Task> itemChanged = entitySubscription.itemChanged;
             itemChanged.listen((task) => newTaskName = task.name);
-
-            // Send the `RenameTask` command.
             var renameTaskCmd = RenameTask()
                 ..id = taskId
                 ..name = 'New task name';
-            await client.post(requestFactory.command().create(renameTaskCmd));
+            var renameTaskRequest = client.command(renameTaskCmd);
+            var taskRenamedEvents = renameTaskRequest.observeEvents(TaskRenamed());
+            renameTaskRequest.post();
 
-            // Verify the event is actually fired.
+            var taskRenamedEvent = await taskRenamedEvents.first;
+            expect(taskRenamedEvent, equals(taskId));
             expect(newTaskName, equals(renameTaskCmd.name));
-
             entitySubscription.unsubscribe();
         });
     });
