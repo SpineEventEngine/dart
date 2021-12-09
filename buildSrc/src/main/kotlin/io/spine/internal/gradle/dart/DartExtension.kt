cbe6e0be94b08926bc4ef92339615390640c6067
yevhenii.nadtochii <yevhenii.nadtochii@teamdev.com>
2021-12-09 17:58:31 +0200

    Document `DartExtension`

--- buildSrc/src/main/kotlin/io/spine/internal/gradle/dart/DartExtension.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/gradle/dart/DartExtension.kt
@@ -33,7 +33,113 @@ import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.findByType
 
 /**
- * Configures [DartExtension].
+ * Configures [DartExtension] that facilitates configuration of Gradle tasks and plugins
+ * to build Dart projects.
+ *
+ * The whole structure of the extension looks as follows:
+ *
+ * ```
+ * dart {
+ *     environment {
+ *         // ...
+ *     }
+ *     tasks {
+ *         // ...
+ *     }
+ *     plugins {
+ *         // ...
+ *     }
+ * }
+ * ```
+ *
+ * ### Environment
+ *
+ * One of the main features of this extension is [DartEnvironment]. Environment describes a module
+ * itself and used tools with their input/output files.
+ *
+ * The extension is shipped with a pre-configured environment. So, no pre-configuration is required.
+ * Most properties in [DartEnvironment] have calculated defaults right in the interface.
+ * Only two properties need explicit override.
+ *
+ * The extension defines them as follows:
+ *
+ *  1. [DartEnvironment.projectDir] –> `project.projectDir`.
+ *  2. [DartEnvironment.projectName] —> `project.name`.
+ *
+ * There are two ways to modify the environment:
+ *
+ *  1. Modify [DartEnvironment]  interface directly. Go with this option when it is a global change
+ *     that should affect all projects which use this extension.
+ *  2. Use [DartExtension.environment] scope — for temporary and custom overridings.
+ *
+ * An example of a property overriding:
+ *
+ * ```
+ * dart {
+ *     environment {
+ *         integrationTestDir = projectDir.resolve("tests")
+ *     }
+ * }
+ * ```
+ *
+ * Please note, environment should be set up firstly to have the effect on the parts
+ * of the extension that use it.
+ *
+ * ### Tasks and Plugins
+ *
+ * The spirit of tasks configuration in this extension is extracting the code that defines and
+ * registers tasks into extension functions upon `DartTasks` in `buildSrc`. Those extensions should
+ * be named after a task it registers or a task group if several tasks are registered at once.
+ * Then this extension is called in a project's `build.gradle.kts`.
+ *
+ * `DartTasks` and `DartPlugins` scopes extend [DartContext] which provides access
+ * to the current [DartEnvironment] and shortcuts for running `pub` tool.
+ *
+ * Below is the simplest example of how to create a primitive `printPubVersion` task.
+ *
+ * Firstly, a corresponding extension function should be defined in `buildSrc`:
+ *
+ * ```
+ * fun DartTasks.printPubVersion() =
+ *     register<Exec>("printPubVersion") {
+ *         pub("--version")
+ *     }
+ * ```
+ *
+ * Secondly, in a project's `build.gradle.kts` this extension is called:
+ *
+ * ```
+ * dart {
+ *     tasks {
+ *         printPubVersion()
+ *     }
+ * }
+ * ```
+ *
+ * An extension function is not restricted to register exactly one task. If several tasks can
+ * be grouped into a logical bunch, they should be registered together:
+ *
+ * ```
+ * fun DartTasks.build() {
+ *     assembleDart()
+ *     testDart()
+ *     generateCoverageReport()
+ * }
+ *
+ * private fun DartTasks.assembleDart() = ...
+ *
+ * private fun DartTasks.testDart() = ...
+ *
+ * private fun DartTasks.generateCoverageReport() = ...
+ * ```
+ *
+ * This section is mostly dedicated to tasks. But tasks and plugins are configured
+ * in a very similar way. So, everything above is also applicable to plugins. More detailed
+ * guides can be found in docs to `DartTasks` and `DartPlugins`.
+ *
+ * @see [ConfigurableDartEnvironment]
+ * @see [DartTasks]
+ * @see [DartPlugins]
  */
 fun Project.dart(configuration: DartExtension.() -> Unit) {
     extensions.run {
@@ -46,46 +152,13 @@ fun Project.dart(configuration: DartExtension.() -> Unit) {
 /**
  * Scope for performing Dart-related configuration.
  *
- * Here is the listing of available aspects to configure:
- *
- *  1. Environment - [ConfigurableDartEnvironment];
- *  2. Tasks - [DartTasks].
- *
- * ### Environment
- *
- * The scope is shipped with the [default values][DartExtension.defaultEnvironment]
- * for [DartEnvironment] based on Dart conventions. Those values can be overwritten
- * through the [DartExtension.environment].
- *
- * An example of overwriting the default Dart environment value:
- *
- * ```
- * dart {
- *     environment {
- *         publicationDirectory = "${defaultEnvironment.publicationDirectory}_DRY_RUN"
- *     }
- * }
- * ```
- *
- * ### Tasks
- *
- * The scope provides its own [task container][DartTasks] within which all Dart-related tasks
- * should be registered and configured.
- *
- * ```
- * dart {
- *     tasks {
- *         ...
- *     }
- * }
- * ```
+ * @see [dart]
  */
 open class DartExtension(project: Project) {
 
     private val environment = ConfigurableDartEnvironment(
         object : DartEnvironment {
             override val projectDir = project.projectDir
-            override val buildDir = project.buildDir
             override val projectName = project.name
         }
     )
@@ -97,15 +170,18 @@ open class DartExtension(project: Project) {
      * Overrides default values of [DartEnvironment].
      *
      * Please note, environment should be set up firstly to have the effect on the parts
-     * of the extension that depend on it.
+     * of the extension that use it.
      */
     fun environment(overridings: ConfigurableDartEnvironment.() -> Unit) =
         environment.run(overridings)
 
     /**
+     * Configures [Dart-related plugins][DartPlugins].
+     */
+    fun plugins(configurations: DartPlugins.() -> Unit) = plugins.run(configurations)
+
+    /**
      * Configures [Dart-related tasks][DartTasks].
      */
     fun tasks(configurations: DartTasks.() -> Unit) = tasks.run(configurations)
-
-    fun plugins(configurations: DartPlugins.() -> Unit) = plugins.run(configurations)
 }
