7b2907c3488c1a81d62f7c02f7f25c6b4c52ca16
Alex Tymchenko <alex.tymchenko@teamdev.com>
2023-03-13 17:28:26 +0000

    Allow to customize `HttpClient` via `HttpTranslator`.

--- client/lib/src/http_client.dart
+++ client/lib/src/http_client.dart
@@ -26,10 +26,13 @@
 
 import 'dart:async';
 import 'dart:convert';
+import 'dart:html';
 
 import 'package:http/http.dart' as http;
 import 'package:protobuf/protobuf.dart';
 import 'package:spine_client/src/url.dart';
+
+import 'json.dart';
 
 const _base64 = Base64Codec();
 const _protobufContentType = {'Content-Type': 'application/x-protobuf'};
@@ -39,19 +42,68 @@ const _protobufContentType = {'Content-Type': 'application/x-protobuf'};
 class HttpClient {
 
     final String _baseUrl;
+    final HttpTranslator _translator;
 
-    HttpClient(this._baseUrl) {
-        ArgumentError.checkNotNull(_baseUrl, 'base URL');
-    }
+    HttpClient(String baseUrl) : this.withTranslator(baseUrl, HttpTranslator());
+
+    HttpClient.withTranslator(String baseUrl, HttpTranslator translator)
+        : this._translator = ArgumentError.checkNotNull(translator, 'HTTP translator'),
+          this._baseUrl = ArgumentError.checkNotNull(baseUrl, 'base URL');
+
 
     /// Sends an HTTP POST request at the given path with the given message as request body.
     ///
     /// The given [path] will be concatenated with the [_baseUrl].
     ///
     Future<http.Response> postMessage(String path, GeneratedMessage message) {
-        var bytes = message.writeToBuffer();
+        String preparedBody = _translator.body(message);
         var url = Url.from(_baseUrl, path).asUri;
-        var response = http.post(url, body: _base64.encode(bytes), headers: _protobufContentType);
+        var response = http.post(url, body: preparedBody, headers: _translator.headers());
         return response;
+    }
+
+    Future<T> postAndTranslate<T extends GeneratedMessage>
+        (String path, GeneratedMessage msg, T destination) {
+        var response = postMessage(path, msg);
+        return response.then((r) => _translator.translate(r.body, destination));
+    }
+}
+
+/// Serves to configure HTTP requests for posting, and translates the corresponding responses.
+///
+/// This is an extension point for users of [Client] API. Custom [HttpTranslator]
+/// implementation, once set, may serve as an adapter to the circumstances,
+/// under which the server-side of the application is running.
+///
+class HttpTranslator {
+
+    /// Transforms the given [message] into [String] which is used as HTTP POST body
+    /// by [postMessage].
+    ///
+    /// By default, transforms the message into bytes, and encodes it with Base64.
+    ///
+    String body(GeneratedMessage message) {
+        var bytes = message.writeToBuffer();
+        var result = _base64.encode(bytes);
+        return result;
+    }
+
+    /// Returns HTTP request headers to use in [postMessage].
+    ///
+    /// By default, returns `{'Content-Type': 'application/x-protobuf'}`.
+    ///
+    Map<String, String> headers() => _protobufContentType;
+
+    /// Translates the HTTP response body into an object of type [T].
+    ///
+    /// By default, parses the response body as JSON.
+    ///
+    /// Parameters:
+    /// - [responseBody] body of a successful HTTP response
+    /// - [destination] an instance of Proto message into which the translated data is to be merged
+    ///
+    T translate<T extends GeneratedMessage>(String responseBody, T destination) {
+        parseInto(destination, responseBody);
+        return destination;
     }
 }
