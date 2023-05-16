dfceafd3bbaf947dcd8ce5e97dfe268d4af493f4
Nick Dolhii <mykola.dolhii@teamdev.com>
2023-05-16 12:24:59 +0200

    Upgrade `web` `1.9.0-SNAPSHOT.12` -> `1.9.0-SNAPSHOT.13`.

--- version.gradle.kts
+++ version.gradle.kts
@@ -38,7 +38,7 @@ val dart = "1.9.0-SNAPSHOT.12"
  * Versions of the Spine libraries that this library depends on.
  */
 val base = "1.9.0-SNAPSHOT.6"
-val web = "1.9.0-SNAPSHOT.12"
+val web = "1.9.0-SNAPSHOT.13"
 
 project.extra.apply {
     this["versionToPublish"] = dart
